800bcfa98f1a78cc6cb4f1f019da4171c42e7e1f
gottomy2 <igottomy@gmail.com>
2020-11-03 00:06:04 +0100

    Changed detected color to blue, Added center of object detection, And simple connection between the objects

--- lab03/zad3.py
+++ lab03/zad3.py
@@ -16,17 +16,11 @@ while True:
         break
 
     _, imageFrame = cap.read()
-
-    # Convert the imageFrame in
-    # BGR(RGB color space) to
-    # HSV(hue-saturation-value)
-    # color space
     hsvFrame = cv.cvtColor(imageFrame, cv.COLOR_BGR2HSV)
 
-    # Set range for red color and define mask
-    red_lower = np.array([136, 87, 111], np.uint8)
-    red_upper = np.array([180, 255, 255], np.uint8)
-    red_mask = cv.inRange(hsvFrame, red_lower, red_upper)
+    blue_lower = np.array([94, 80, 2], np.uint8)
+    blue_upper = np.array([120, 255, 255], np.uint8)
+    blue_mask = cv.inRange(hsvFrame, blue_lower, blue_upper)
 
     # Morphological Transform, Dilation
     # for each color and bitwise_and operator
@@ -34,22 +28,33 @@ while True:
     # to detect only that particular color
     kernal = np.ones((5, 5), "uint8")
 
-    # For red color
-    red_mask = cv.dilate(red_mask, kernal)
-    res_red = cv.bitwise_and(imageFrame, imageFrame,red_mask);
+    #For blue color:
+    blue_mask = cv.dilate(blue_mask, kernal)
+    res_blue = cv.bitwise_and(imageFrame, imageFrame,blue_mask)
 
-    # Creating contour to track red color
-    contours, hierarchy = cv.findContours(red_mask,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-
+        # Creating contour to track blue color
+    centers = []
+    contours, hierarchy = cv.findContours(blue_mask,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
     for pic, contour in enumerate(contours):
         area = cv.contourArea(contour)
-        if (area > 300):
+        if area > 500:
             x, y, w, h = cv.boundingRect(contour)
-            imageFrame = cv.rectangle(imageFrame, (x, y),(x + w, y + h),(0, 0, 255), 2)
-            cv.putText(imageFrame, "Red Color", (x, y),cv.FONT_HERSHEY_SIMPLEX, 1.0,(0, 0, 255))
+            imageFrame = cv.rectangle(imageFrame, (x, y),(x + w, y + h),(255, 0, 0), 2)
+            cv.putText(imageFrame, "Blue", (x, y), cv.FONT_ITALIC, 1.0, (255, 0, 0))
+
+            M = cv.moments(contours[0])
+            cy = int(M['m01'] / M['m00'])
+            cx = int(M['m10'] / M['m00'])
+            centers.append([cx, cy])
+            imageFrame = cv.circle(imageFrame, (cx, cy), 7, (255, 255, 255), -1)
 
 
-    cv.imshow("Multiple Red Color Detection", imageFrame)
+            #cv.line(imageFrame,(centers[0][0], centers[0][1]),(x + int(w / 2), y + int(h / 2)), (0, 0, 0), 3,cv.LINE_AA)
+            #cv.line(imageFrame, (centers[0][0], centers[0][1]), (cx,cy), (0, 0, 0), 3,cv.LINE_AA)
+        if len(centers) == 2:
+            cv.line(imageFrame, (cx, cy), (x + int(w / 2), y + int(h / 2)), (0, 0, 0), 3,cv.LINE_AA)
+            #cv.line(imageFrame, (centers[0][0], centers[0][1]), (centers[1][0], centers[1][1]), (0, 0, 0), 3,cv.LINE_AA)
+    cv.imshow("Multiple Color Detection", imageFrame)
 
     #Break if 'esc' has been pressed
     if cv.waitKey(33) == 27:
