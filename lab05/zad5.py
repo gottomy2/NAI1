8abba7316311bc070a12558033cf04e793b9d574
gottomy2 <igottomy@gmail.com>
2020-11-17 14:22:15 +0100

    Zmiana w kodzie tak aby zliczać lączna ilosc samochodow przechodzacych przez kadr

--- lab05/zad5.py
+++ lab05/zad5.py
@@ -4,24 +4,51 @@ cascade_src = 'cars.xml'
 video_src = 'video.avi'
 cap = cv.VideoCapture(video_src)
 
-def detectCars(frame):
-    car_cascade = cv.CascadeClassifier(cascade_src)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
-    count = 0;
-    for (x, y, w, h) in cars:
-        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
-        count += 1
-        print(count)
-    cv.imshow('video', frame)
+min_contour_width = 50  # 40
+min_contour_height = 50  # 40
+approx = 10  # 10
+line_height = 100  # 550
+matches = []
+count = 0
+
+def getCenter(x, y, w, h):
+    x1 = int(w / 2)
+    y1 = int(h / 2)
+
+    cx = x + x1
+    cy = y + y1
+
+    return (cx, cy)
 
 while True:
     ret, frame = cap.read()
     if (type(frame) == type(None)):
         break
 
-    detectCars(frame)
-    if cv.waitKey(33) == 27:
+    #Using the cars cascade to identify cars:
+    car_cascade = cv.CascadeClassifier(cascade_src)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+
+    for (x, y, w, h) in cars:
+        contour_valid = (w >= min_contour_width) and (h >= min_contour_height)
+        if not contour_valid:
+            continue
+        cv.rectangle(frame, (x - approx, y - approx), (x + w + approx, y + h + approx), (0), 2)
+        center = getCenter(x, y, w, h)
+        matches.append(center)
+        cv.circle(frame, center, 5, (255, 0, 0), -1)
+    for (x, y) in matches:
+        if y < (line_height + approx) and y > (line_height - approx):
+            count += 1
+            matches.remove((x, y))
+            print(count)
+    cv.line(frame, (0, line_height), (3000, line_height), (0, 0, 255), 2)
+    cv.putText(frame, "Count: " + str(count), (10, 50), cv.FONT_ITALIC, 1, (0, 0, 0), 2)
+
+    cv.imshow('video', frame)
+
+    if cv.waitKey(1) == 27:
         break
 
 cv.destroyAllWindows()
